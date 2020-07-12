9af4f62e7fae9f77113c73bf61ba9414b756776a
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-07-12 12:02:57 +0530

    Create matchingCharacterRanges.java

---
+++ matchingCharacterRanges.java
@@ -0,0 +1,28 @@
+/*
+Write a RegEx that will match a string satisfying the following conditions:
+
+The string's length is >=5.
+The first character must be a lowercase English alphabetic character.
+The second character must be a positive digit. Note that we consider zero to be neither positive nor negative.
+The third character must not be a lowercase English alphabetic character.
+The fourth character must not be an uppercase English alphabetic character.
+The fifth character must be an uppercase English alphabetic character.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-z][1-9][^a-z][^A-Z][A-Z]"); // Use \\ instead of using \ 
+    
+    }
+}
+
+class Regex_Test {
